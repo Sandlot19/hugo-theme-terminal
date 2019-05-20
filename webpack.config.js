1c63aa8e69ecdd096bec7f0de477542e8fe1b2d8
Jacob Rutherford <jacobrutherford88@gmail.com>
2019-05-20 15:46:46 -0400

    add purple :)

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
     "blue.css": join("source", "css", "color", "blue.css"),
     "green.css": join("source", "css", "color", "green.css"),
     "pink.css": join("source", "css", "color", "pink.css"),
+    "purple.css": join("source", "css", "color", "purple.css"),
   },
   output: {
     filename: "[name]",
